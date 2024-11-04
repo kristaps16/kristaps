2401befdb3f571b08e9e250963f19409b70a22bb
kristaps16 <kristapsmikelsons3584@gmail.com>
2024-11-04 12:55:58 +0200

    Add files via upload

---
+++ uzd1.pyx
@@ -0,0 +1,24 @@
+try:
+
+x = int(input ('Enter x:'))
+
+y = int(input ('Enter y:'))
+
+
+except ValueError:
+
+print ('x or y = int')
+
+A = ( 2 + x ) / ( x * y )
+
+print (A)
+
+B = ( 5 x * 3 ) - ( x**2 ) + ( 7 * x ) - ( 6 )
+
+print (B)
+
+C = (x y * 0.5)
+
+print (C)
+
+D = (2 ** x y) / (5 y)
