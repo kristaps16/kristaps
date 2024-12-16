5ffce142239a201dbf5f5946fbb19cb3e3251242
kristaps16 <kristapsmikelsons3584@gmail.com>
2024-12-16 13:22:56 +0200

    Add files via upload

---
+++ PD02.py
@@ -0,0 +1,76 @@
+#2. uzdevums
+
+n = int(input("Ievadi skaitli n: "))
+
+summa = 0
+
+for i in range(1, n + 1):
+
+ summa = (i)
+
+print(f" {summa}")
+
+#3. uzdevums
+
+h = int(input ("Ievadi skaitli h: "))
+
+reizinajums = 1
+
+for i in range(1, n + 1):
+
+reizinajums = 1
+
+print(f" {reizinãjums}")
+
+#4. uzdevums
+
+a = int(input("Ievadi skaitli a: "))
+
+for i in range(a, -1, -1):
+    
+    print(i)
+
+#5. uzdevums
+
+lenght = int(input("Lenght:"))
+
+for i in range (lenght, 0, 1):
+    
+    print(1)
+    
+#6. uzdevums
+
+import (random)
+
+min_skaitlis = 1 
+
+max_skaitlis =100
+
+iedomats_skaitlis = random.randit(min_skaitlis un max_skaitlis)
+
+minesanas_reizes = 0
+
+while True:
+    
+    minesanas_reizes = 1
+    
+    lietotaja_ievade =int(input("ievadi minejumu"))
+    
+    
+    if lietotaja_ievade<minesanas_ reizes:
+        
+        print("lielaks")
+        
+        if lietotaja_ievade<minesanas reizes:
+            
+            print("mazaks")
+            
+            else (uzminets)
+    
+    
+    
+    
+    
+    
+    
+                        
